6c84cd8e99956f16f2e92479dac2ff62c7968987
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-01-24 01:25:00 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = '';
+var url = 'https://wa.me';
 try {
-if(url == '') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://wa.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
